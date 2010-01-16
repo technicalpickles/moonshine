6b2466a92a9fa155a97ab18f83edbd09860c1f48
Joshua Nichols <josh@technicalpickles.com>
2010-01-16 02:34:53 -0500

    Hey, actually generates!

--- lib/rails_generators/moonshine/moonshine_generator.rb
+++ lib/rails_generators/moonshine/moonshine_generator.rb
@@ -3,15 +3,14 @@ require 'rbconfig'
 class MoonshineGenerator < Rails::Generators::Base
   argument :name, :optional => true, :default => 'application'
 
+  def self.source_root
+    @_moonshine_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+  end
+  
+
   def manifest
     directory 'app/manifests'
-    directory 'app/manifests/templates'
-    template  'moonshine.rb', "app/manifests/#{file_name}.rb"
-    directory 'app/manifests/templates'
-    template  'readme.templates', 'app/manifests/templates/README'
     directory 'config'
-    template  'moonshine.yml', "config/moonshine.yml"
-    template  'gems.yml', "config/gems.yml", :assigns => { :gems => gems }
     
     intro = <<-INTRO
     
@@ -21,7 +20,7 @@ After the Moonshine generator finishes don't forget to:
 Use this file to manage configuration related to deploying and running the app: 
 domain name, git repos, package dependencies for gems, and more.
 
-- Edit app/manifests/#{file_name}.rb
+- Edit app/manifests/#{manifest_name}.rb
 Use this to manage the configuration of everything else on the server:
 define the server 'stack', cron jobs, mail aliases, configuration files 
 
@@ -30,30 +29,11 @@ define the server 'stack', cron jobs, mail aliases, configuration files
   end
 
 protected
-  def file_name
-    @file_name ||= name.downcase.underscore + "_manifest"
+  def manifest_name
+    @manifest_name ||= name.downcase.underscore + "_manifest"
   end
 
   def klass_name
-    @klass_name ||= @file_name.classify
+    @klass_name ||= manifest_name.classify
   end
-
-  def gems
-    gem_array = returning Array.new do |hash|
-      Rails.configuration.gems.map do |gem|
-        hash = { :name => gem.name }
-        hash.merge!(:source => gem.source) if gem.source
-        hash.merge!(:version => gem.requirement.to_s) if gem.requirement
-        hash
-      end if Rails.respond_to?( 'configuration' )
-    end
-    if (RAILS_GEM_VERSION rescue false)
-      gem_array << {:name => 'rails', :version => RAILS_GEM_VERSION }
-    else
-      gem_array << {:name => 'rails'}
-    end
-    gem_array
-  end
-
-  
 end
